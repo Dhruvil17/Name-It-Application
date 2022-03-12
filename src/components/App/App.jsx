cfd8cb5c568b29d13fe076f80635fc085844f9d3
dhruvil <dhruvilsoni1703@gmail.com>
2022-03-13 01:28:54 +0530

    use hook concept

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../Header/Header';
 import ResultsContainer from '../ResultsContainer/ResultsContainer';
 import SearchBox from '../SearchBox/SearchBoc';
@@ -6,32 +6,51 @@ import './App.css';
 
 const name = require('@rstacruz/startup-name-generator');
 
-class App extends React.Component {
-    state = {
-        headerText: 'Name It!',
-        headerExpanded: true,
-        suggestedNames: [],
+const App = () => {
+    const [headerText, setHeaderText] = useState('Name It!');
+    const [headerExpanded, setHeaderExpanded] = useState(false);
+    const [suggestedNames, setSuggestedNames] = useState([]);
+
+    const handleInputChange = (inputText) => {
+        setHeaderExpanded(!inputText);
+        setSuggestedNames(inputText ? name(inputText) : []);
     };
 
-    handleInputChange = (inputText) => {
-        this.setState({
-            headerExpanded: !inputText,
-            suggestedNames: inputText ? name(inputText) : [],
-        });
-    };
+    return (
+        <div>
+            <Header headerExpanded={headerExpanded} headTitle={headerText} />
+            <SearchBox onInputChange={handleInputChange} />
+            <ResultsContainer suggestedNames={suggestedNames} />
+        </div>
+    );
+};
 
-    render() {
-        return (
-            <div>
-                <Header
-                    headerExpanded={this.state.headerExpanded}
-                    headTitle={this.state.headerText}
-                />
-                <SearchBox onInputChange={this.handleInputChange} />
-                <ResultsContainer suggestedNames={this.state.suggestedNames} />
-            </div>
-        );
-    }
-}
+// class App extends React.Component {
+//     state = {
+//         headerText: 'Name It!',
+//         headerExpanded: true,
+//         suggestedNames: [],
+//     };
+
+//     handleInputChange = (inputText) => {
+//         this.setState({
+//             headerExpanded: !inputText,
+//             suggestedNames: inputText ? name(inputText) : [],
+//         });
+//     };
+
+//     render() {
+//         return (
+//             <div>
+//                 <Header
+//                     headerExpanded={this.state.headerExpanded}
+//                     headTitle={this.state.headerText}
+//                 />
+//                 <SearchBox onInputChange={this.handleInputChange} />
+//                 <ResultsContainer suggestedNames={this.state.suggestedNames} />
+//             </div>
+//         );
+//     }
+// }
 
 export default App;
